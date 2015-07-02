06c35e36d3f6eed4268d12347f08cf9cab0edcb1
Samtoto <lisensen163@gmail.com>
2015-07-02 22:23:17 +0800

    message submit template

--- pages/submit.php
+++ pages/submit.php
@@ -30,51 +30,21 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?php echo $cfg['url'];?>index.php">Project name</a>
+            <a class="navbar-brand" href="<?php echo $cfg['url'];?>">Project name</a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="<?php echo $cfg['url'];?>index.php">Home</a></li>
+              <li class="active"><a href="<?php echo $cfg['url'];?>">Home</a></li>
               <li><a href="<?php echo $cfg['url'];?>index.php">View Message</a></li>
               <li><a href="<?php echo $cfg['url'];?>submit.php">Leave a Message</a></li>
-              <!-- <li><a href="#">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li> -->
+             
             </ul>
-            <!-- <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-              <li><a href="../navbar-static-top/">Static top</a></li>
-              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-            </ul> -->
+            
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
       </nav>
     
-    <div class="jumbotron">
-
-     <?php
-
-        foreach ($msgAll as $key => $value) {
-            echo "{$value->id}==>{$value->title}<br />";
-
-            echo "{$value->content}";
-
-            echo "<hr>";
-        }
-
-    ?>
-        
-    </div>
+    
 
 
     </div> <!-- /container -->
---
+++ submit.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+
+
+
+
+require_once("pages/submit.php")
+?>
